fa6d9dcb26a281eac796ed448e93c7ac93cd20ec
SamsonGulma <sams0ngultek@gmail.com>
2024-10-30 13:39:45 +0000

    Oct 30, 2024, 4:39 PM

--- back-parts/routes/goalRoutes.js
+++ back-parts/routes/goalRoutes.js
@@ -5,9 +5,21 @@ const router = express.Router()
 
 
 router.get("/", (requests, responds) => {
-    responds.status(200).json({message: "Welcome to the Uncle Sam App"})
+    responds.status(200).json({message: "Get Goals"})
 });
 
+
+router.post("/", (requests, responds) => {
+    responds.status(200).json({message: "Set Goal"})
+});
+
+router.put("/:id", (requests, responds) => {
+    responds.status(200).json({message: `Update Goal ${requests.params.id}`})
+});
+
+router.delete("/:id", (requests, responds) => {
+    responds.status(200).json({message: `Delete Goal ${requests.params.id}`})
+});
 
 
 module.exports = router
