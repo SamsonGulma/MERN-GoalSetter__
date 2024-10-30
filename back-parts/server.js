62a2702067e3032d9ff95d33bb4dc93dbc219a7a
SamsonGulma <sams0ngultek@gmail.com>
2024-10-30 15:32:27 +0000

    Oct 30, 2024, 6:32 PM

--- back-parts/server.js
+++ back-parts/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes.js'))
 
