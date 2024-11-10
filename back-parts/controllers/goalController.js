a10c958e8426d18269ec957f8b191b55d12887e7
SamsonGulma <sams0ngultek@gmail.com>
2024-11-10 05:34:06 +0300

    correcting some unexpected output

--- back-parts/controllers/goalController.js
+++ back-parts/controllers/goalController.js
@@ -1,19 +1,29 @@
 const asyncHandler = require('express-async-handler')
-const Goal = require('../models/goalModel.js')
+const Goal = require('../models/goalModel.js');
+const { request } = require('express');
 
 const getGoals = asyncHandler(async (requests, responds) => {
+    const goals = await Goal.find()
+
     if (!requests.body.text) {
         responds.status(400)
         throw new Error('Please add a text field; the text field value is empty.')
     }
     else {
         console.log(requests.body)
-        responds.status(200).json({ 'message': 'Get Goals' });
+        responds.status(200).json(goals);
     }
 });
 
-const setGoals = asyncHandler(async (requests, responds) => {
-    responds.status(200).json({ 'message': 'Set Goal' })
+const setGoal = asyncHandler(async (requests, responds) => {
+     if (!requests.body.text) {
+        responds.status(400)
+        throw new Error('Please add a text field; the text field value is empty.')
+     }
+    const goal = await Goal.create({
+        text: requests.body.text,
+    })
+    responds.status(200).json(goal)
 });
 
 const updateGoal = asyncHandler(async (requests, responds) => {
@@ -26,7 +36,7 @@ const deleteGoal = asyncHandler(async (requests, responds) => {
 
 module.exports = {
     getGoals,
-    setGoals,
+    setGoal,
     updateGoal,
     deleteGoal,
 };
